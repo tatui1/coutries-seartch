d42a481d2e7ba3514456088d6a4331246725131b
tatui1 <kamila.lumazova@compasscollege.art>
2026-03-01 00:13:55 +0600

    данные стран

--- src/types.ts
+++ src/types.ts
@@ -3,3 +3,22 @@ export interface IContryShort {
     alpha3Code: string;
     independent: boolean;
 }
+export interface ICountryFull {
+    name: string;
+    alpha3Code: string;
+    capital: string;
+    population: number;
+    area: number;
+    region: string;
+    flags: {
+        svg: string;
+        png: string;
+    };
+    currencies: Array<{
+        name: string;
+    }>;
+    languages: Array<{
+        name: string;
+    }>;
+    borders: string[];
+}
