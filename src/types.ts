5533b65e0eb585dbe3e88f8dcc810bbf5fba0748
tatui1 <kamila.lumazova@compasscollege.art>
2026-03-03 06:16:14 +0600

    добавляем данные

--- src/types.ts
+++ src/types.ts
@@ -2,23 +2,8 @@ export interface IContryShort {
     name: string;
     alpha3Code: string;
     independent: boolean;
-}
-export interface ICountryFull {
-    name: string;
-    alpha3Code: string;
     capital: string;
-    population: number;
-    area: number;
     region: string;
-    flags: {
-        svg: string;
-        png: string;
-    };
-    currencies: Array<{
-        name: string;
-    }>;
-    languages: Array<{
-        name: string;
-    }>;
-    borders: string[];
+    population: number;
+    flag: string;
 }
